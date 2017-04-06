dee07cb4a5be846f5d8004c96878ec9465b07f4e
c-awatsu <cist.b213.c.awatsu@gmai.com>
2017-04-06 21:25:06 +0900

    ignoreに.idea以下を含めた stageのtitleを設定した

--- src/optGroupComboBox/Java/Main.java
+++ src/optGroupComboBox/Java/Main.java
@@ -12,6 +12,7 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         AnchorPane anchorPane = FXMLLoader.load(getClass().getResource("/optGroupComboBox/fxml/OptGroupComboBox.fxml"));
         Scene root = new Scene(anchorPane);
+        primaryStage.setTitle("optgroupComboBox");
         root.getStylesheets().add(getClass().getResource("/optGroupComboBox/css/optGroup.css").toString());
         primaryStage.setScene(root);
         primaryStage.show();
